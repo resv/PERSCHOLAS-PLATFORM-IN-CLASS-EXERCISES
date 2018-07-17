7bd1ff3220a35e15492405dcedd916deed449d0d
Adam Kim <akim456@gmail.com>
2018-07-17 12:20:05 -0400

    Day 2 Slide 16 Exercises completed

---
+++ DayTwoSlidePage16.java
@@ -0,0 +1,22 @@
+package inClassExercises;
+
+public class DayTwoSlidePage16 {
+
+	public static void main(String[] args) {
+//		QUESTION
+//			ask: Create a for while that runs from 100 down to but not
+//			including 0 that displays the value of your variable in each
+//			cycle. Have the loop decrease by 10.
+
+		int i = 100;
+		
+		while (i > 1) {
+			System.out.println(i);
+			i = i - 10;
+			
+		}
+		
+		
+	}
+
+}
