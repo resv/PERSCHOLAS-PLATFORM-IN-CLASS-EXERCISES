0178a46102a6b3b9d72a95ddd3fe045a1c1f1cc7
Adam Kim <akim456@gmail.com>
2018-07-18 10:53:36 -0400

    added another in class exercise regarding arrays

--- DayThreeSlidePage27.java
+++ DayThreeSlidePage27.java
@@ -5,12 +5,15 @@ public class DayThreeSlidePage27 {
 
 		public static void main(String[] args) {
 			
-			// DAY 3 SLIDE 27 SORT EVEN NUMBERS FROM ARRAYS 
+			// DAY 3 SLIDE 27
+			// GRAB EVEN numbers that exists in both arrays and put them in a new array
 			
 			int[] listA = {5,10,15,20,25,30,35};
 			int[] listB = {4,5,1,10,22,20,30};
 			int[] listC = new int[8];
+			int[] listD = new int[4];
 			int n = 0;
+			
 			Arrays.fill(listC, 0);
 			
 			for (int i = 0; i < listA.length; i++) {
@@ -30,6 +33,24 @@ public class DayThreeSlidePage27 {
 			
 			System.out.println("NEW ARRAY listC in sequential order by index position from listA and listB");
 			System.out.println("listC = " + Arrays.toString(listC));
+			
+			int m = 0;
+			for (int a = 0; a < (listC.length / 2); a++) {
+				for (int b = 4; b < listC.length; b++) {
+					if (listC[a] == listC[b]) {
+						listD[m] = listC[b];
+						m++;
+					}
+				}
+			}
+			System.out.println();
+			System.out.println("Final array that has common even numbers from both arrays");
+			System.out.println("listD = " + Arrays.toString(listD));
+			
+			
+			
+			
+			
 		}
 
 	}		
